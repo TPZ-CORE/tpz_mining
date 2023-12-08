cfd69d1d03afa7b1aa38239558aea0a97f85d9da
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2023-12-09 01:24:28 +0200

    Add files via upload

---
+++ tpz_mining/locales.lua
@@ -0,0 +1,11 @@
+Locales = {}
+
+Locales = {
+
+    ['MINING']               = "Start Mining",
+    ['CANNOT_CARRY']         = "You don't have enough space to carry more mineral ores.",
+    ['FOUND_NOTHING']        = "~e~You haven't found anything",
+
+    ['NO_DURABILITY']        = "~e~The Shovel you have been using, unfortunately seems to be useless.",
+    ['SUCCESSFULLY_FOUND']   = "You have found \n+%s %s.",
+}
