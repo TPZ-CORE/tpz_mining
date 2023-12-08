fe8b403a1e904d82b0c7f9f43a377bb17ecc3989
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2023-12-09 01:42:29 +0200

    Update locales.lua

--- tpz_mining/locales.lua
+++ tpz_mining/locales.lua
@@ -6,6 +6,6 @@ Locales = {
     ['CANNOT_CARRY']         = "You don't have enough space to carry more mineral ores.",
     ['FOUND_NOTHING']        = "~e~You haven't found anything",
 
-    ['NO_DURABILITY']        = "~e~The Shovel you have been using, unfortunately seems to be useless.",
+    ['NO_DURABILITY']        = "~e~The Pickaxe you have been using, unfortunately seems to be useless.",
     ['SUCCESSFULLY_FOUND']   = "You have found \n+%s %s.",
 }
